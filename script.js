b9e38496173128ff271c401375f721f3c2c3f17e
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-06-11 14:51:36 +0530

    feat: Create functions that populate display when buttons are clicked
    Added functions that populate the display when a button is clicked by first creating a function that updates the display when called and then created the event listeners that are triggered when a button is clicked and then calls for the function to update the display, then another function to clear the display was created and assigned it to the event listener attached to the clear button.

--- script.js
+++ script.js
@@ -37,3 +37,92 @@ function operate(operand1,operator,operand2){
     }
 }
 
+let displayValue = "";
+
+function updateDisplay(number){
+    displayValue += number
+    const para = document.querySelector('.text')
+    para.textContent = displayValue
+}
+
+function clearDisplay(){
+    displayValue = '';
+    const para = document.querySelector('.text')
+    para.textContent = displayValue;
+}
+
+const num1 = document.querySelector('#num1');
+num1.addEventListener('click', () => {
+    updateDisplay('1');
+});
+
+const num2 = document.querySelector('#num2');
+num2.addEventListener('click', () => {
+    updateDisplay('2');
+});
+
+const num3 = document.querySelector('#num3');
+num3.addEventListener('click', () => {
+    updateDisplay('3');
+});
+
+const num4 = document.querySelector('#num4');
+num4.addEventListener('click', () => {
+    updateDisplay('4');
+});
+
+const num5 = document.querySelector('#num5');
+num5.addEventListener('click', () => {
+    updateDisplay('5');
+});
+
+const num6 = document.querySelector('#num6');
+num6.addEventListener('click', () => {
+    updateDisplay('6');
+});
+
+const num7 = document.querySelector('#num7');
+num7.addEventListener('click', () => {
+    updateDisplay('7');
+});
+
+const num8 = document.querySelector('#num8');
+num8.addEventListener('click', () => {
+    updateDisplay('8');
+});
+
+const num9 = document.querySelector('#num9');
+num9.addEventListener('click', () => {
+    updateDisplay('9');
+});
+
+const num0 = document.querySelector('#num0');
+num0.addEventListener('click', () => {
+    updateDisplay('0');
+});
+
+
+const addButton = document.querySelector('#add')
+addButton.addEventListener('click',() =>{
+    updateDisplay('+')
+});
+
+const subtractButton = document.querySelector('#subtract')
+subtractButton.addEventListener('click',() =>{
+    updateDisplay('-')
+});
+
+const multiplyButton = document.querySelector('#multiply')
+multiplyButton.addEventListener('click',() =>{
+    updateDisplay('x')
+});
+
+const divideButton = document.querySelector('#divide')
+divideButton.addEventListener('click',() =>{
+    updateDisplay('÷')
+});
+
+const clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', () =>{
+    clearDisplay()
+})
