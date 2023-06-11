95e80e3f66c59913dbc178e2b72b2cf578b3bbc6
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-06-11 12:51:13 +0530

    feat: Create new function
    Created a new function that calls one of the previous function depending on the user input. It takes 3 parameters- operand1, operand2 & operator - and then prompts the user to assign a value to each of the 3 variables. The value of operand1 & operand2 gets turned into an integer using the parseInt() function. And then a function is called depending on the value for operator.

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function divide(a,b){
     return a/b
 }
 
+let operand1;
+let operand2;
+let operator;
+
+function operate(operand1,operator,operand2){
+    operand1 = parseInt(prompt('Number 1?'))
+    operand2 = parseInt(prompt('Number 2?'))
+    operator = prompt('What do you wanna do with these numbers?')
+
+    if (operator === '+' ){
+        alert(add(operand1,operand2))
+    }
+    else if (operator === '-' ){
+        alert(subtract(operand1,operand2))
+    }
+    else if (operator === 'x' ){
+        alert(multiply(operand1,operand2))
+    }
+    else if (operator === '/' ){
+        alert(divide(operand1,operand2))
+    }
+}
+
